2056a24123a88890f19e741b4f065199d2b6e1c0
gruyuri <gruyuri@yandex.ru>
2021-02-19 14:00:34 +0100

    EventHandler on NavigationMenu

--- DashboardWpf/DashboardWpf/Views/NavigationMenuView.xaml.cs
+++ DashboardWpf/DashboardWpf/Views/NavigationMenuView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using DashboardWpf.Core.Models;
+using DashboardWpf.ViewModels;
+using System.Windows.Controls;
 
 namespace DashboardWpf.Views
 {
@@ -10,6 +12,22 @@ namespace DashboardWpf.Views
         public NavigationMenuView()
         {
             InitializeComponent();
+        }
+
+        private void OnSelectedItemChanged(object sender, System.Windows.RoutedPropertyChangedEventArgs<object> e)
+        {
+            ModuleUI newModel = e.NewValue as ModuleUI;
+            NavigateToModel(newModel);
+        }
+
+        private void NavigateToModel(ModuleUI model)
+        {
+            var dataContext = this.DataContext as NavigationMenuViewModel;
+
+            if (model != null)
+            {
+                dataContext.NavigateCommand.Execute(model.ModuleName);
+            }
         }
 
     }
