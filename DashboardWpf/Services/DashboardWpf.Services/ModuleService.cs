cfedc8dcce5ccd8a7ad861e2d914745d3cb60b92
gruyuri <gruyuri@yandex.ru>
2021-02-24 18:15:12 +0100

    Clear Modules - actual

--- DashboardWpf/Services/DashboardWpf.Services/ModuleService.cs
+++ DashboardWpf/Services/DashboardWpf.Services/ModuleService.cs
@@ -21,31 +21,16 @@ namespace DashboardWpf.Services
                 }
             });
 
-            list.Add(new ModuleUI()
-            {
-                DisplayName = "Module A",
-                Items = new List<ModuleUI>()
-                {
-                    new ModuleUI() { DisplayName = "Module A - SubItem 01" },
-                    new ModuleUI() { DisplayName = "Module A - SubItem 02" },
-                    new ModuleUI() { DisplayName = "Module A - SubItem 03" }
-                }
-            });
-
-            list.Add(new ModuleUI()
-            {
-                DisplayName = "Module B",
-                Items = new List<ModuleUI>()
-                {
-                    new ModuleUI() { DisplayName = "Module B - xxxx 1" },
-                    new ModuleUI() { DisplayName = "Module B - xxxx 2" },
-                    new ModuleUI() { DisplayName = "Module B - xxxx 3" },
-                    new ModuleUI() { DisplayName = "Module B - xxxx 4" },
-                    new ModuleUI() { DisplayName = "Module B - xxxx 5" },
-                    new ModuleUI() { DisplayName = "Module B - xxxx 6" },
-                    new ModuleUI() { DisplayName = "Module B - xxxx 7" }
-                }
-            });
+            //list.Add(new ModuleUI()
+            //{
+            //    DisplayName = "Module A",
+            //    Items = new List<ModuleUI>()
+            //    {
+            //        new ModuleUI() { DisplayName = "Module A - SubItem 01" },
+            //        new ModuleUI() { DisplayName = "Module A - SubItem 02" },
+            //        new ModuleUI() { DisplayName = "Module A - SubItem 03" }
+            //    }
+            //});
 
             return list;
         }
